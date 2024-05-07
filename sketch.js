bed04150ee40263808ac157bc8037cffccf5d9e4
Johnson <jj20050919@gmail.com>
2024-05-06 21:24:59 -0400

    selection tool mostly done

--- sketch.js
+++ sketch.js
@@ -11,6 +11,10 @@ let lines = [];
 
 let mouseMath;
 let mouseState;
+let selected = false;
+
+let expressions = new Map();
+
 
 // Tools:
 // "linear" : 1
@@ -24,16 +28,11 @@ let tool = "bezier";
 class Line {
     constructor(type, ...args) {
         this.type = type;
-
         if (type === "bezier") {
-            this.c1 = args[0];
-            this.c2 = args[1];
-            this.c3 = args[2];
-            this.c4 = args[3];
+            this.cp = [args[0], args[1], args[2], args[3]];
         }
         else if (type === "linear") {
-            this.c1 = args[0];
-            this.c2 = args[1];
+            this.cp = [args[0], args[1]];
         }
 
     }
@@ -51,8 +50,8 @@ function setup() {
     calculatorDiv.position(0, 0);
 
     options = {
-        // expressions: false,
-        // settingsMenu: false,
+        expressions: false,
+        settingsMenu: false,
         lockViewport: true,
     };
 
@@ -60,21 +59,28 @@ function setup() {
     calc = Desmos.GraphingCalculator(elt, options );
     
 
-    calc.setExpression({ id: 'bezierPrevCP', latex: '[]', color: "#5a6ef2" });
-    calc.setExpression({ id: 'bezierCurCP', latex: '[]', color: "#5a6ef2" });
-    calc.setExpression({ id: 'bezierLine', latex: '', color: "#5a6ef2" });
+    setExp({ id: 'bezierPrevCP', latex: '[]', color: "#5a6ef2", lines:true  });
+    setExp({ id: 'bezierCurCP', latex: '[]', color: "#5a6ef2", lines:true  });
+    setExp({ id: 'bezierLine', latex: '0', color: "#5a6ef2" });
 
-    calc.setExpression({ id: 'linearCP', latex: '[]', color: "#5a6ef2" });
-    calc.setExpression({ id: 'linearLine', latex: '', color: "#5a6ef2" });
+    setExp({ id: 'linearCP', latex: '[]', color: "#5a6ef2" });
+    setExp({ id: 'linearLine', latex: '', color: "#5a6ef2" });
 
 }
 
 function draw() {
     background(310,30,20); 
 
+    push();
+    strokeWeight(2);
+    textSize(50);
+    stroke(255);
+    text(`tool: ${tool}`, width*0.8, height/2);
+    pop();
+
+
+
     mouseMath = cOp((x) => round(x, 3), calc.pixelsToMath({x:mouseX, y:mouseY}));
-
-
 
     if (mouseState === "bezier") {
 
@@ -82,54 +88,72 @@ function draw() {
         bezierCurCP[0] = cOp((x1, x2) => 2 * x1 - x2, bezierCurCP[1], mouseMath);
         
         let exp = bezierCurCP.map((val) => Dpoint(val))
-        calc.setExpression({ id: `bezierCurCP`, latex: `[${exp.toString()} ]`, lines:true });
+        setExp({ id: `bezierCurCP`, latex: `[${exp.toString()} ]`});
 
         exp = bezierPrevCP.map((val) => Dpoint(val))
-        calc.setExpression({ id: `bezierPrevCP`, latex: `[${exp.toString()} ]`, lines:true });
+        setExp({ id: `bezierPrevCP`, latex: `[${exp.toString()} ]`});
 
         if ( bezierPrevCP.length ) {
-        calc.setExpression({ id: `bezierLine`, latex: Dbezier(bezierPrevCP[0], bezierPrevCP[1], bezierCurCP[0], bezierCurCP[1]) });
+        setExp({ id: `bezierLine`, latex: Dbezier(bezierPrevCP[0], bezierPrevCP[1], bezierCurCP[0], bezierCurCP[1]) });
         }
-
     }
-
     else if (mouseState === "linear") {
         linearCP[1] = mouseMath;
 
         let exp = linearCP.map((val) => Dpoint(val));
-        calc.setExpression({ id: `linearCP`, latex: `[${exp.toString()} ]`, lines:true });
+        setExp({ id: `linearCP`, latex: `[${exp.toString()} ]`, lines:true });
+    }
+    else if (mouseState === "selectBCP") {
+        let minI;
+        let minDist = Infinity;
+        for (let i = 0; i < 4; i++) {
+            newDist = dist(selected.cp[i].x, selected.cp[i].y, mouseMath.x, mouseMath.y);
+            if (newDist < minDist) {
+                minI = i;
+                minDist = newDist;
+            }
+        }
+        selected.cp[minI] = mouseMath;
     }
 
 
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
         if (line.type === "bezier") {
-            calc.setExpression({ id: `bezier${i}`, latex: Dbezier(line.c1, line.c2, line.c3, line.c4), color: "#000000" });
+            if (selected === line) {
+                setExp({ id: `bezierPrevCP`, latex: `[${Dpoint(line.cp[0])}, ${Dpoint(line.cp[1])} ]`});
+                setExp({ id: `bezierCurCP`, latex: `[${Dpoint(line.cp[2])}, ${Dpoint(line.cp[3])} ]` });
+                setExp({ id: `bezier${i}`, latex: Dbezier(...line.cp), color: "#5a6ef2"});
+            }
+            else {
+                setExp({ id: `bezier${i}`, latex: Dbezier(...line.cp), color: "#000000" });
+            }
         }
         else if (line.type === "linear") {
-            calc.setExpression({ id: `linear${i}`, latex: Dline(line.c1, line.c2)});
+            setExp({ id: `linear${i}`, latex: Dline(...line.cp), color: "#000000"});
         }
     }
+
+
+
+
+    if ( keyIsDown(18) ) {
+        calc.updateSettings({lockViewport:false})   
+    }
+    else {
+        calc.updateSettings({lockViewport:true}) 
+    }
+
 
     if (keyIsDown(32)) {
-        if (tool === "bezier") {
-            console.log("wah")
-            if ( bezierCurCP.length && bezierPrevCP.length) {
-                lines.push(new Line("bezier", bezierPrevCP[0], bezierPrevCP[1], bezierCurCP[0], bezierCurCP[1]));
-            }
-
-            bezierPrevCP = [];
-            bezierCurCP = [];
-            calc.setExpression({ id: 'bezierPrevCP', latex: '[]'});
-            calc.setExpression({ id: 'bezierCurCP', latex: '[]'});
-            mouseState = false;
-            tool = "select";
-        }
+        resetTool();
     }
     else if (keyIsDown(49)) {
+        resetTool();
         tool = "linear";
     }
     else if (keyIsDown(50)) {
+        resetTool();
         tool = "bezier";
     }
     
@@ -139,6 +163,9 @@ function draw() {
 }
 
 function mousePressed() {
+    if (calc.settings.lockViewport === false) {
+        return;
+    } 
     if (tool === "bezier") {
 
         if ( bezierCurCP.length ) {
@@ -164,6 +191,33 @@ function mousePressed() {
         linearCP.push( mouseMath );
         mouseState = "linear";
     }
+    else if (tool === "select") {
+        
+        if (selected.type === "bezier") {
+            for (let p of selected.cp) {
+                if (dist(p.x, p.y, mouseMath.x, mouseMath.y) < 0.4) {
+                    mouseState = "selectBCP";
+                    return;
+                }
+            }
+        }
+
+        let minDist = Infinity;
+
+        for (let i = 0; i < lines.length; i++) {
+            newDist = lineDist(lines[i],mouseMath)
+            if (newDist < minDist) {
+                minDist = newDist;
+                closestLine = lines[i];
+            }
+        }
+        if (minDist < 0.5) {
+            selected = closestLine;
+        }
+        else {
+            mouseState = "select";
+        }
+    }
 }
 
 function mouseReleased() {
@@ -171,7 +225,6 @@ function mouseReleased() {
         if ( bezierCurCP.length && bezierPrevCP.length) {
 
             lines.push(new Line("bezier", bezierPrevCP[0], bezierPrevCP[1], bezierCurCP[0], bezierCurCP[1]));
-
 
             bezierPrevCP[0] = bezierCurCP[1];
             bezierPrevCP[1] = bezierCurCP[2];
@@ -186,9 +239,96 @@ function mouseReleased() {
         linearCP = []
         mouseState = false;
     }
+    if (tool === "select") {
+        mouseState = false;
+    }
 }
 
 
+function resetTool() {
+    
+    if (tool === "bezier") {
+
+        if ( bezierCurCP.length && bezierPrevCP.length) {
+            lines.push(new Line("bezier", bezierPrevCP[0], bezierPrevCP[1], bezierCurCP[0], bezierCurCP[1]));
+        }
+
+        bezierPrevCP = [];
+        bezierCurCP = [];
+        setExp({ id: 'bezierPrevCP', latex: '[]'});
+        setExp({ id: 'bezierCurCP', latex: '[]'});
+        setExp({ id: 'bezierLine', latex: '' });
+    }
+    else if (tool === "linear") {
+        if ( linearCP.length) {
+            lines.push(new Line("linear", linearCP[0], linearCP[1]));
+        }
+        linearCP = []
+        setExp({ id: 'linearCP', latex: '[]', color: "#5a6ef2" });
+    }
+    else if (tool === "select") {
+        bezierCurCP = [];
+        bezierPrevCP = [];
+        selected = false;
+        setExp({ id: 'bezierPrevCP', latex: '[]'});
+        setExp({ id: 'bezierCurCP', latex: '[]'});
+        setExp({ id: 'bezierLine', latex: '' });
+    }
+    mouseState = false;
+    tool = "select";
+}
+
+/**
+ * @param {Line} line - line object
+//  * @param {Vector} pt 
+ */
+function lineDist(line, pt) { // Returns a coordinate struct
+    if (line.type === "bezier") {
+        let scans = 20;
+        let eval = (t) => (cOp((x1,x2,x3,x4) => (1-t)**3 * x1 + 3 * t * (1-t)**2 * x2 + 3 * t**2 * (1-t) * x3 + t**3 * x4, ...line.cp));
+        let minDist = Infinity;
+        let minT = 0;
+
+        for (let i = 0; i <= 20; i++) {
+            cur = eval(i/scans);
+            if (dist(cur.x, cur.y, pt.x, pt.y) < minDist) {
+                minDist = dist(cur.x, cur.y, pt.x, pt.y);
+                minT = i/scans;
+            }
+        }
+
+        
+
+        let ans = minSearch((t) => dist(eval(t).x, eval(t).y, pt.x, pt.y), max(0, minT - 1/scans), min(1, minT + 1/scans), min(1- 1/scans, max(1/scans, minT)));
+        let pos = eval(ans);
+        console.log(minT, ans);
+        console.log(minDist, dist(pos.x, pos.y, pt.x, pt.y));
+        
+        return dist(pos.x, pos.y, pt.x, pt.y);
+    }
+}
+
+function setExp(exp) {
+    calc.setExpression(exp);
+    expressions.set(exp.id, exp);
+}
+
+// Returns an input t value
+function minSearch(fun, smin, smax, smid) {
+    let scans = 10;
+    for (let i = 0; i <= scans; i++) {
+        if (fun((smin + smid)/2) < fun((smax + smid)/2)) {
+            smax = smid;
+            smid = (smin + smax)/2;
+        }
+        else {
+            smin = smid;
+            smid = (smin + smax)/2;
+        }
+    }
+    return smin;
+
+}
 
 function cOp(fun, ...coords) {
     let cx = coords.map((c) => c.x);
@@ -196,14 +336,14 @@ function cOp(fun, ...coords) {
     return {x: fun( ...cx ), y: fun( ...cy ) }
 }
 
-    // Compile to desmos code
 
+// Compile to desmos code
 function Dbezier(c1, c2, c3, c4) {
     return `(1-t)^3 \\cdot ${Dpoint(c1)} + 3t(1-t)^2 \\cdot ${Dpoint(c2)} + 3t^2(1-t) \\cdot ${Dpoint(c3)} + t^3 ${Dpoint(c4)} `
 }
 
 function Dline(c1, c2) {
-    if (abs(atan2(c1.y - c2.y, c1.x - c2.x)) > 1.55 && abs(atan2(c1.y - c2.y, c1.x - c2.x)) < 1.6) {
+    if (abs(atan2(c1.y - c2.y, c1.x - c2.x)) > 1.55 && abs(atan2(c1.y - c2.y, c1.x - c2.x))  < 1.6) {
         return `x = ${(c1.x-c2.x)/(c1.y-c2.y)}(y-${c1.y}) + ${c1.x} \\left\\{ ${min(c1.y,c2.y)} \\leq y \\leq ${max(c1.y,c2.y)} \\right\\}`;
     }
     else {
@@ -214,3 +354,4 @@ function Dline(c1, c2) {
 function Dpoint(coords) {
     return `(${coords.x},${coords.y})`
 }
+
