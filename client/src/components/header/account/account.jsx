7f0cae3b6980f1704a465b5b07c8344e3d11af5a
Johnson <jj20050919@gmail.com>
2025-01-01 18:19:39 -0700

    deployed database connnection works

--- client/src/components/header/account/account.jsx
+++ client/src/components/header/account/account.jsx
@@ -4,8 +4,8 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 
-// const db = "https://desmos-renderer.onrender.com/"
-const db = "http://localhost:5050"; 
+const db = "https://desmos-renderer.onrender.com"
+// const db = "http://localhost:5050"; 
 
 function Account() {
     // State to control the dropdown visibility
