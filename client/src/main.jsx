a2af82d9dc648fbfc6a7a75d280c0203cc7d0598
Johnson <jj20050919@gmail.com>
2025-01-01 20:32:57 -0700

    fix attempt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,12 +7,17 @@ import './index.css'
 
 const root = createRoot(document.getElementById('root'));
 
+const redirectUri =
+  process.env.NODE_ENV === "production"
+    ? "https://maaiv.github.io/"
+    : "http://localhost:3000";
+
 root.render(
     <Auth0Provider
         domain="dev-5gze5rgawusihxof.us.auth0.com"
         clientId="iCgc65SsBkPwBzhcOr9Qy0M9739q8mQU"
         authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: redirectUri
         }}
     >
         <App />
